47e659552213dbfefd4a61f0dfdf3a7e016a6d91
sttagami2 <sttagami2@gmail.com>
2021-02-14 23:27:06 +0900

    padding

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -10,6 +10,41 @@ class _NextPageState extends State<NextPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  child: Text('ID'),
+                ),
+                Container(
+                  width: 20,
+                  child: Text(':'),
+                ),
+                Text('毎回同じIDを表示'),
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  child: Text('PW'),
+                ),
+                Container(
+                  width: 20,
+                  child: Text(':'),
+                ),
+                Text('毎回同じパスワードを表示'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
